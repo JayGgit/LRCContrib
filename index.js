3d6a202328abbf79ea9f22030980aa3665bf9f4d
JayG <jayg@jayg.top>
2025-01-11 10:57:01 -0800

    Start of the lyric view function.

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html'); // Sends index.html
 });
 
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/public/view.html'); // Sends view.html
+})
+
 app.get('/api/search', (req, res) => {
     try {
         const response = axios.get('https://api.genius.com/search', { // Start GET request to genius search api
@@ -35,9 +39,42 @@ app.get('/api/search', (req, res) => {
                     tempRes.push({ // Push onto tempRes array as json for api use
                         "name": `${songInfo.title}`, // Song name
                         "artistName": `${songInfo.artist_names}`, // Artist name
-                        "image": `${songInfo.song_art_image_url}` // Image URL
+                        "image": `${songInfo.song_art_image_url}`, // Image URL
+                        "geniusID": `${songInfo.id}` // Image URL
                     })
                 }
+            }
+            else {
+                console.log('No song found'); // Womp Womp
+                console.log(response.data)
+            }
+            res.send(tempRes) // Sends completed song list
+        })
+    } catch (error) {
+        console.error('Error fetching song details:', error);
+    }
+})
+
+app.get('/api/getId', (req, res) => {
+    try {
+        const response = axios.get('https://api.genius.com/songs/' + req.query.id, { // Start GET request to genius search api
+            params: {
+            },
+            headers: {
+                "Lrclib-Client": "LRCContrib v0.1.0a (https://github.com/JayGgit/LRCContrib)", // Not really needed for genius, but included anyways
+                "Authorization": `Bearer ${genuisAPI}` // API token. Specified in .env as GENUISAPIKEY
+            }
+        })
+        .then(data => {
+            tempRes = []; // All songs get pushed onto this array // Outdated but too lazy to change cause copy pasted
+            if (data.data) { // If stuff actually exists
+                const songInfo = data.data.response.song; // Song info
+                tempRes.push({ // Push onto tempRes array as json for api use
+                    "name": `${songInfo.title}`, // Song name
+                    "artistName": `${songInfo.artist_names}`, // Artist name
+                    "image": `${songInfo.song_art_image_url}`, // Image URL
+                    "geniusID": `${songInfo.id}` // Image URL
+                })
             }
             else {
                 console.log('No song found'); // Womp Womp
