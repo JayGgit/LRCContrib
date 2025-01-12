5e8eebab9ccd43ac639662ad98ba8ce424b926be
JayG <jayg@jayg.top>
2025-01-11 21:56:36 -0800

    Unfinished Token handling

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const path = require("path")
 const axios = require('axios')
+const crypto = require('crypto');
+const { Worker } = require('worker_threads');
+const os = require('os');
+const {verify} = require('hcaptcha');
+
 
 require('dotenv').config();
 const genuisAPI = process.env.GENUISAPIKEY // For genius API use
+const hCaptchaSecret = process.env.HCAPTCHA // For hCaptcha use
+
+// Challenge solver
+
+function verify_nonce(result, target) {
+    if (result.length != target.length) return false;
+
+    for (let i = 0; i < result.length - 1; i++) {
+        if (result[i] > target[i]) return false;
+        else if (result[i] < target[i]) break;
+    }
+
+    return true;
+}
+
+function solve(prefix, target_hex) {
+    let nonce = 0;
+    const target = Buffer.from(target_hex, 'hex');
+
+    console.log('');
+    while (true) {
+        if (nonce % 100000 === 0)
+            console.log(`Solver - Nonce (~100000): ${nonce}`);
+        const input = `${prefix}${nonce}`;
+        const hashed = crypto.createHash('sha256').update(input).digest();
+        if (verify_nonce(hashed, target)) {
+            break;
+        } else {
+            nonce++;
+        }
+    }
+
+    return { nonce: nonce, prefix: prefix };
+}
+
+// Captcha validator
+
+function validateCaptcha(token) {
+    verify(hCaptchaSecret, token)
+        .then((data) => {
+        if (data.success === true) {
+            return(true)
+        } else {
+            return(false)
+        }
+        })
+        .catch(console.error);
+}
+
+// Start express server
 
 const PORT = 8080;
 
@@ -20,7 +76,7 @@ app.get('/view', (req, res) => {
     res.sendFile(__dirname + '/public/view.html'); // Sends view.html
 })
 
-app.get('/api/search', (req, res) => {
+app.get('/api/search', async (req, res) => {
     try {
         const response = axios.get('https://api.genius.com/search', { // Start GET request to genius search api
             params: {
@@ -86,6 +142,17 @@ app.get('/api/getId', (req, res) => {
         console.error('Error fetching song details:', error);
     }
 })
+
+app.post('/api/getToken', bodyParser.text(), function (req, res) {
+    console.log(req.body)
+    if (validateCaptcha(req.body.token)) {
+        console.log("valid")
+        res.send(solve(req.query.prefix, req.query.target))
+    } else {
+        console.log("invalid")
+        res.sendStatus(400)
+    }
+});
 
 // 404 Not Found
 app.use((req, res, next) => {
